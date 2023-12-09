d27f92c6aeb3f2f9fa4c63b274ccc672a71b0649
ebicep <wmah314@gmail.com>
2023-12-08 23:19:40 -0500

    fix: forge icon + commands not being registered

--- forge/src/main/kotlin/com/ebicep/chatplus/events/forge/ClientCommandRegistration.kt
+++ forge/src/main/kotlin/com/ebicep/chatplus/events/forge/ClientCommandRegistration.kt
@@ -4,10 +4,14 @@ import com.ebicep.chatplus.config.ConfigScreen
 import com.mojang.brigadier.Command
 import net.minecraft.commands.Commands
 import net.minecraftforge.client.event.RegisterClientCommandsEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.fml.common.Mod
 
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 object ClientCommandRegistration {
 
+    @SubscribeEvent
     fun registerCommands(event: RegisterClientCommandsEvent) {
         val commandNode = event.dispatcher.register(
             Commands.literal("chatplus")
--- forge/src/main/kotlin/com/ebicep/chatplus/forge/ChatPlusForge.kt
+++ forge/src/main/kotlin/com/ebicep/chatplus/forge/ChatPlusForge.kt
@@ -4,7 +4,6 @@ package com.ebicep.chatplus.forge
 import com.ebicep.chatplus.ChatPlus
 import com.ebicep.chatplus.MOD_ID
 import dev.architectury.platform.forge.EventBuses
-import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
 
@@ -15,7 +14,6 @@ object ChatPlusForge {
     init {
         EventBuses.registerModEventBus(MOD_ID, MOD_BUS)
         ChatPlus.init()
-
     }
 
 }
