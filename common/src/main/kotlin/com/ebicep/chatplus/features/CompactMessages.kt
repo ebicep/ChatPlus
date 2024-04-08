fcc62a1bb6d69558c615a126771560dedeb0843e
ebicep <wmah314@gmail.com>
2024-04-07 23:10:09 -0400

    fix: CompactMessages timesRepeated retaining parent style

--- common/src/main/kotlin/com/ebicep/chatplus/features/CompactMessages.kt
+++ common/src/main/kotlin/com/ebicep/chatplus/features/CompactMessages.kt
@@ -3,13 +3,18 @@ package com.ebicep.chatplus.features
 import com.ebicep.chatplus.events.EventBus
 import com.ebicep.chatplus.features.chattabs.ChatTabAddNewMessageEvent
 import net.minecraft.ChatFormatting
-import net.minecraft.network.chat.ComponentContents
-import net.minecraft.network.chat.FormattedText
-import net.minecraft.network.chat.MutableComponent
-import net.minecraft.network.chat.Style
+import net.minecraft.network.chat.*
 import java.util.*
 
 object CompactMessages {
+
+    private val COMPACT_STYLE = Style.EMPTY
+        .withColor(TextColor.fromLegacyFormat(ChatFormatting.GRAY))
+        .withBold(false)
+        .withItalic(false)
+        .withUnderlined(false)
+        .withStrikethrough(false)
+        .withObfuscated(false)
 
     init {
         EventBus.register<ChatTabAddNewMessageEvent> {
@@ -26,11 +31,7 @@ object CompactMessages {
             if (content.equals(it.componentWithTimeStamp)) {
                 lastMessage.timesRepeated++
                 guiMessage.content.siblings.removeIf { component -> component.contents is LiteralContentsIgnored }
-                guiMessage.content.siblings.add(literalIgnored(" (${lastMessage.timesRepeated})").withStyle {
-                    it.withColor(
-                        ChatFormatting.GRAY
-                    )
-                })
+                guiMessage.content.siblings.add(literalIgnored(" (${lastMessage.timesRepeated})").withStyle(COMPACT_STYLE))
                 // remove previous displayed message and update it
                 for (i in displayedMessages.size - 1 downTo 0) {
                     val displayedMessage = displayedMessages[i]
