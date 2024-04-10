0c76bf0a7443d212ce7654e61406e189a1e1b64b
ebicep <wmah314@gmail.com>
2024-04-10 00:29:37 -0400

    fix: key down error if not bound

--- common/src/main/kotlin/com/ebicep/chatplus/util/KeyUtil.kt
+++ common/src/main/kotlin/com/ebicep/chatplus/util/KeyUtil.kt
@@ -6,6 +6,7 @@ import net.minecraft.client.Minecraft
 object KeyUtil {
 
     fun InputConstants.Key.isDown(): Boolean {
+        if (value == -1) return false
         val window = Minecraft.getInstance().window ?: return false
         return InputConstants.isKeyDown(window.window, value)
     }
