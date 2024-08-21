af13699c64244be095862a040f4f4de93bc3b94f
ebicep <wmah314@gmail.com>
2024-08-21 15:18:31 -0400

    feat: optimized timestamp memory usage

--- common/src/main/kotlin/com/ebicep/chatplus/features/TimestampMessages.kt
+++ common/src/main/kotlin/com/ebicep/chatplus/features/TimestampMessages.kt
@@ -15,35 +15,42 @@ import java.time.format.DateTimeFormatter
 
 object TimestampMessages {
 
+    private var lastTime: String = ""
+    private var lastTimestamp: Component = Component.empty()
+    private var lastTimestampNewLine: Component = Component.empty()
+
     init {
         EventBus.register<ChatTabAddNewMessageEvent>({ 5 }) {
             if (Config.values.chatTimestampMode == TimestampMode.NONE) {
+                it.mutableComponent = it.rawComponent.copy()
                 return@register
+            }
+            val currentTime = getCurrentTime()
+            if (lastTime != currentTime) {
+                lastTime = currentTime
+                lastTimestamp = getTimestamp(false)
+                lastTimestampNewLine = getTimestamp(true)
             }
             it.mutableComponent = getTimeStampedMessage(it.rawComponent)
         }
     }
 
     private fun getTimeStampedMessage(component: Component): MutableComponent {
-        if (Config.values.chatTimestampMode == TimestampMode.NONE) {
-            return component.copy() as MutableComponent
-        }
         val componentWithTimeStamp: MutableComponent = Component.empty()
         val timestampedHoverComponents = HashSet<Any>()
         component.toFlatList().forEach {
             val flatComponent = it as MutableComponent
             if (flatComponent.style.hoverEvent == null) {
                 flatComponent.withStyle {
-                    val hoverValue = getTimestamp(false)
-                    timestampedHoverComponents.add(hoverValue)
-                    it.withHoverEvent(HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverValue))
+                    timestampedHoverComponents.add(lastTimestamp)
+                    it.withHoverEvent(HoverEvent(HoverEvent.Action.SHOW_TEXT, lastTimestamp))
                 }
             } else {
                 when (flatComponent.style.hoverEvent?.action) {
                     HoverEvent.Action.SHOW_TEXT -> {
                         val hoverValue = flatComponent.style.hoverEvent?.getValue(HoverEvent.Action.SHOW_TEXT)
                         if (hoverValue != null && !timestampedHoverComponents.containsReference(hoverValue)) {
-                            hoverValue.siblings.add(getTimestamp(true))
+                            hoverValue.siblings.add(lastTimestampNewLine)
                             timestampedHoverComponents.add(hoverValue)
                         }
                     }
@@ -51,7 +58,7 @@ object TimestampMessages {
                     HoverEvent.Action.SHOW_ENTITY -> {
                         val hoverValue = flatComponent.style.hoverEvent?.getValue(HoverEvent.Action.SHOW_ENTITY)
                         if (hoverValue != null && !timestampedHoverComponents.containsReference(hoverValue.tooltipLines)) {
-                            hoverValue.tooltipLines.add(getTimestamp(false))
+                            hoverValue.tooltipLines.add(lastTimestamp)
                             timestampedHoverComponents.add(hoverValue.tooltipLines)
                         }
                     }
@@ -64,14 +71,8 @@ object TimestampMessages {
     }
 
     private fun getTimestamp(newLine: Boolean): Component {
-        return literalIgnored((if (newLine) "\n" else "") + "Sent at ")
-            .withStyle {
-                it.withColor(ChatFormatting.GRAY)
-            }
-            .append(Component.literal(getCurrentTime())
-                .withStyle {
-                    it.withColor(ChatFormatting.YELLOW)
-                })
+        return literalIgnored((if (newLine) "\n" else "") + "Sent at ").withStyle(ChatFormatting.GRAY)
+            .append(Component.literal(getCurrentTime()).withStyle(ChatFormatting.YELLOW))
             .append(Component.literal("."))
     }
 
