565f7efc6872c69e074d813e08f5c53cefbd8e70
ebicep <wmah314@gmail.com>
2024-08-25 00:48:35 -0400

    fix: AlignMessage not calculating width correctly

--- common/src/main/kotlin/com/ebicep/chatplus/features/AlignMessage.kt
+++ common/src/main/kotlin/com/ebicep/chatplus/features/AlignMessage.kt
@@ -3,13 +3,17 @@ package com.ebicep.chatplus.features
 import com.ebicep.chatplus.config.Config
 import com.ebicep.chatplus.config.EnumTranslatableName
 import com.ebicep.chatplus.events.EventBus
+import com.ebicep.chatplus.features.chattabs.ChatTabAddDisplayMessageEvent
 import com.ebicep.chatplus.features.chattabs.ChatTabGetMessageAtEvent
+import com.ebicep.chatplus.features.internal.Debug.debug
+import com.ebicep.chatplus.hud.ChatManager
 import com.ebicep.chatplus.hud.ChatRenderLineTextEvent
 import com.ebicep.chatplus.hud.ChatRenderer
 import com.ebicep.chatplus.util.GraphicsUtil.translate0
 import kotlinx.serialization.Serializable
 import net.minecraft.client.Minecraft
 import net.minecraft.network.chat.Component
+import net.minecraft.util.FormattedCharSequence
 import kotlin.math.max
 
 object AlignMessage {
@@ -17,22 +21,49 @@ object AlignMessage {
     init {
         EventBus.register<ChatRenderLineTextEvent> {
             val alignment = it.chatWindow.messageAlignment
-            var xTranslation = alignment.translation(it.chatWindow.renderer, it.text)
+            var xTranslation = alignment.translation(it.chatWindow.renderer, it.chatPlusGuiMessageLine.line.content)
             if (alignment == Alignment.RIGHT && Config.values.scrollbarEnabled) {
                 xTranslation -= max(0, Config.values.scrollbarWidth)
             }
             it.guiGraphics.pose().translate0(x = xTranslation)
         }
+        EventBus.register<ChatTabAddDisplayMessageEvent>({ -1 }) {
+            val chatWindow = it.chatWindow
+            if (chatWindow.messageAlignment == Alignment.CENTER) {
+                it.maxWidth -= 5
+            }
+        }
         EventBus.register<ChatTabGetMessageAtEvent> {
             val chatTab = it.chatTab
             val chatWindow = chatTab.chatWindow
             val messageLine = chatTab.getMessageAtLineRelative(it.chatX, it.chatY) ?: return@register
-            it.chatX -= chatWindow.messageAlignment.translation(chatWindow.renderer, messageLine.content)
+            it.chatX -= chatWindow.messageAlignment.translation(chatWindow.renderer, messageLine.line.content)
+        }
+        EventBus.register<ChatRenderLineTextEvent> {
+            if (!debug) {
+                return@register
+            }
+            if (!ChatManager.isChatFocused()) {
+                return@register
+            }
+            val renderer = it.chatWindow.renderer
+            val guiGraphics = it.guiGraphics
+            val pose = guiGraphics.pose()
+//            pose.createPose {
+//                pose.translate0(z = 5000)
+//                guiGraphics.drawString(
+//                    Minecraft.getInstance().font,
+//                    "${Minecraft.getInstance().font.width(it.chatPlusGuiMessageLine.line.content)} | ${getExtraBoldWidth(it.text)}",
+//                    renderer.internalX - 10,
+//                    it.verticalChatOffset - renderer.lineHeight,
+//                    0x00FF00
+//                )
+//            }
         }
     }
 
     @Serializable
-    enum class Alignment(val key: String, val translation: (renderer: ChatRenderer, text: String) -> Double) : EnumTranslatableName {
+    enum class Alignment(val key: String, val translation: (renderer: ChatRenderer, text: FormattedCharSequence) -> Double) : EnumTranslatableName {
         LEFT(
             "chatPlus.chatWindow.messageAlignment.left",
             { _, _ -> 0.0 }
@@ -43,7 +74,7 @@ object AlignMessage {
         ),
         RIGHT(
             "chatPlus.chatWindow.messageAlignment.right",
-            { renderer, text -> (renderer.rescaledWidth - Minecraft.getInstance().font.width(text)).toDouble() }
+            { renderer, text -> (renderer.rescaledWidth - Minecraft.getInstance().font.width(text).toDouble()) }
         ),
 
         ;
